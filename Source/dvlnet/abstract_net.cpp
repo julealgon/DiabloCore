359d55fefb9909c44f3aa24db4491b0449d2f9f3
Juliano Leal Goncalves <julealgon@gmail.com>
2021-09-26 01:59:55 -0300

    🔥 Remove ZeroTier support

--- Source/dvlnet/abstract_net.cpp
+++ Source/dvlnet/abstract_net.cpp
@@ -4,9 +4,6 @@
 #ifndef NONET
 #include "dvlnet/base_protocol.h"
 #include "dvlnet/cdwrap.h"
-#ifndef DISABLE_ZERO_TIER
-#include "dvlnet/protocol_zt.h"
-#endif
 #ifndef DISABLE_TCP
 #include "dvlnet/tcp_client.h"
 #endif
@@ -26,10 +23,6 @@ std::unique_ptr<abstract_net> abstract_net::MakeNet(provider_t provider)
 #ifndef DISABLE_TCP
 	case SELCONN_TCP:
 		return std::make_unique<cdwrap<tcp_client>>();
-#endif
-#ifndef DISABLE_ZERO_TIER
-	case SELCONN_ZT:
-		return std::make_unique<cdwrap<base_protocol<protocol_zt>>>();
 #endif
 	case SELCONN_LOOPBACK:
 		return std::make_unique<loopback>();
