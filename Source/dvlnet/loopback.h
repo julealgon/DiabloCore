f9097b76ba5dc00bf2578c92ab8d3589847e2453
Juliano Leal Goncalves <julealgon@gmail.com>
2021-09-28 00:48:43 -0300

    ♻️ Cleanup 'Loopback' implementation

--- Source/dvlnet/loopback.h
+++ Source/dvlnet/loopback.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <queue>
-#include <string>
 
 #include "storm/storm.h"
 
@@ -9,7 +8,6 @@ namespace devilution {
 namespace net {
 
 typedef std::vector<unsigned char> buffer_t;
-typedef unsigned long provider_t;
 
 class loopback {
 private:
@@ -17,10 +15,10 @@ private:
 	buffer_t message_last;
 
 public:
-	virtual bool SNetReceiveMessage(int *sender, void **data, uint32_t *size);
-	virtual bool SNetSendMessage(int dest, void *data, unsigned int size);
-	virtual bool SNetReceiveTurns(char **data, size_t *size);
-	virtual void SNetGetProviderCaps(struct _SNETCAPS *caps);
+	bool SNetReceiveMessage(int *sender, void **data, uint32_t *size);
+	bool SNetSendMessage(int dest, void *data, unsigned int size);
+	bool SNetReceiveTurns(char **data, size_t *size);
+	void SNetGetProviderCaps(struct _SNETCAPS *caps);
 };
 
 } // namespace net
